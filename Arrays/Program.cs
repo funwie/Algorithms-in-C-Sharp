29dd9a8dd5f2e85eaf0f28c1b08285103fa9f134
Lesley Funwie <funwielesley@outlook.com>
2020-04-27 07:52:34 +0100

    Set Up Test

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,6 +6,8 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int[] arr = { 3, 5, 3, 2, 4 };
+            ArraySolutions<int>.InsertAtFirst(10, arr);
             Console.WriteLine("Hello, Arrays!");
         }
     }
